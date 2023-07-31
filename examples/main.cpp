fdd63e2066af0fcdfa89d6ba6e6be8cc2a951ed8
woXrooX <woXrooX@gmail.com>
2023-07-31 20:24:22 +0500

    file renamed and moved to examples folder

---
+++ examples/main.cpp
@@ -0,0 +1,13 @@
+#include "source/logger.hpp"
+
+int main() {
+
+  woXrooX::Logger::enableLogToFile();
+
+  woXrooX::Log::success("Success");
+  woXrooX::Log::info("Information");
+  woXrooX::Log::warning("Warning");
+  woXrooX::Log::error("Error");
+
+  return 0;
+}
